54b37ac7091e207f37b5951d8069d6dff429b787
PARKCW <PARKCW@22090121-PARKCHANWOOK>
2020-12-21 11:11:37 +0900

    19_싱글톤패턴

---
+++ src/com/javalec/pattern/MainClass.java
@@ -0,0 +1,12 @@
+package com.javalec.pattern;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//ΩÃ±€≈œ∆–≈œ
+		FirstClass fc = new FirstClass();
+
+	}
+
+}
